418a2384a6fb931ef6d09e0c49976590a0389fa6
Cristina Giraldo <cristinagiraldo22@gmail.com>
2025-10-30 20:48:24 -0500

    se modifica reponse por response, se corrige escritura.

--- api_screemplay_petstore/src/tasks/GetPetsByStatus.py
+++ api_screemplay_petstore/src/tasks/GetPetsByStatus.py
@@ -3,14 +3,14 @@ from src.abilities.CallApi import CallApi
 class GetPetsByStatus:
     def __init__(self,status):
         self.status = status
-        self.reponse = None
+        self.response = None
 
 
     def perform_as(self, actor):
         api = actor.ability_to(CallApi)
         endpoint = f"/pet/findByStatus"    
         params = {"status": self.status}
-        self.reponse = api.get(endpoint, params=params)
-        actor.last_response = self.reponse
+        self.response = api.get(endpoint, params=params)
+        actor.last_response = self.response
 
         print(f"Actor {actor.name} obtuvo código {self.response.status_code}")
